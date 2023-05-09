4f2712ef37c7c1bc738aa0d5db841e7cacab2a94
MoneshwarTestSigma <127829016+MoneshwarTestSigma@users.noreply.github.com>
2023-05-09 15:57:45 +0530

    Add files via upload

---
+++ flywayDemoMySQL/flywayDemoMySQL/src/main/java/com/moneshwar/flywayDemoMySQL/FlywayDemoMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.moneshwar.flywayDemoMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayDemoMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayDemoMySqlApplication.class, args);
+	}
+
+}
---
+++ flywayDemoMySQL/flywayDemoMySQL/src/test/java/com/moneshwar/flywayDemoMySQL/FlywayDemoMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneshwar.flywayDemoMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
